4cd43fb774551f2147382dc3e22e9de2de03eb8d
Heng2j <heng2j@gmail.com>
2019-01-07 11:07:20 -0500

    updated run.sh to redirect pids for nohup

--- run.sh
+++ run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # streaming_tweets
-nohup python3 ./src/producer/streaming_tweets.py > ./logs/streaming_tweets.py.out &
-nohup python3 ./src/consumer/consumer_stream.py --streamName TweetsStream --tableName TweetsStreamTweets > ./logs/consumer_stream_TweetsStream.out &
+echo 'streaming_tweets.py: ' > save_pid.txt
+nohup python3 ./src/producer/streaming_tweets.py > ./logs/streaming_tweets.py.out 2>&1 &
+echo $! >> save_pid.txt
 
+nohup python3 ./src/consumer/consumer_stream.py --streamName TweetsStream --tableName TweetsStreamTweets > ./logs/consumer_stream_TweetsStream.out 2>&1 &
+echo $! >> save_pid.txt
 
 # streaming_stock
-nohup python3 ./src/producer/streaming_stock.py > ./logs/streaming_stock.py.out &
-nohup python3 ./src/consumer/consumer_stream.py --streamName StockTradeStream --tableName StockTradesProcessor > ./logs/consumer_stream_StockTradeStream.out &
-
+nohup python3 ./src/producer/streaming_stock.py > ./logs/streaming_stock.py.out 2>&1 &
+echo $! >> save_pid.txt
+nohup python3 ./src/consumer/consumer_stream.py --streamName StockTradeStream --tableName StockTradesProcessor > ./logs/consumer_stream_StockTradeStream.out  2>&1 &
+echo $! >> save_pid.txt
 
 
 
